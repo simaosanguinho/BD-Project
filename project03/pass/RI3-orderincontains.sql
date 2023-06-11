b5b0545c4b2be1c95b3bf826ac3dfa2f30de52f1
Rafael Girão <git@rafael.ovh>
2023-06-11 11:53:49 +0100

    Fix populate with missing attrs

--- project03/pass/RI3-orderincontains.sql
+++ project03/pass/RI3-orderincontains.sql
@@ -2,15 +2,15 @@
 BEGIN;
 
 -- Insert a product
-INSERT INTO product(SKU, name, description, price, ean)
+INSERT INTO product (sku, name, description, price, ean)
 VALUES ('SKU123', 'Product Name', 'Product Description', 10.99, 1234567890123);
 
 -- Create an order
-INSERT INTO orders(order_no, cust_no, date)
+INSERT INTO orders (order_no, cust_no, date)
 VALUES (1, 123, '2023-06-10');
 
 -- Add corresponding entry in the contains table
-INSERT INTO contains(order_no, SKU, qty)
+INSERT INTO contains (order_no, sku, qty)
 VALUES (1, 'SKU123', 10);
 
 -- Commit the transaction
@@ -20,13 +20,13 @@ COMMIT;
 BEGIN;
 
 -- Delete the corresponding entry from the contains table
-DELETE FROM contains WHERE order_no = 1 AND SKU = 'SKU123';
+DELETE FROM contains WHERE order_no = 1 AND sku = 'SKU123';
 
 -- Delete the created order
 DELETE FROM orders WHERE order_no = 1;
 
 -- Delete the inserted product
-DELETE FROM product WHERE SKU = 'SKU123';
+DELETE FROM product WHERE sku = 'SKU123';
 
 -- Commit the transaction
 COMMIT;
