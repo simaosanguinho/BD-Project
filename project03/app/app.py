906a0f9233b556d9aea8bdb9c51bcb619e88bf26
simaosanguinho <simao.sanguinho@gmail.com>
2023-06-11 21:07:24 +0100

    add delete supplier

--- project03/app/app.py
+++ project03/app/app.py
@@ -63,30 +63,235 @@ def order():
     return render_template("order/index.html")
 
 
-@app.route("/product", methods=("GET",))
+@app.route("/product", methods=("GET", "POST"))
 # @app.route("/accounts", methods=("GET",))
-def product():
+def product_index():
     """Product management page."""
     with pool.connection() as conn:
         with conn.cursor(row_factory=namedtuple_row) as cur:
-            products = cur.execute(
+            cur.execute(
                 """
                SELECT SKU, name, description, price, ean
                FROM product
                ORDER BY price DESC;
                """,
                 {},
-            ).fetchall()
+                # prepare=True,
+            )
+            products = cur.fetchmany(10)
             log.debug(f"Found {cur.rowcount} rows.")
     return render_template("product/index.html", products=products)
 
+@app.route("/product/add", methods=("GET", "POST"))
+def product_add():
+    """Add a new product."""
+    if request.method == "POST":
+        sku = request.form["sku"]
+        name = request.form["name"]
+        description = request.form["description"]
+        price = request.form["price"]
+        ean = request.form["ean"]
 
-@app.route("/supplier", methods=("GET",))
-# @app.route("/accounts", methods=("GET",))
-def supplier():
+        error = None
+
+        if not sku:
+            error = "SKU is required."
+
+        if not name:
+            error = "Name is required."
+
+        if not description:
+            error = "Description is required."
+
+        if not price:
+            error = "Price is required."
+
+        if not ean:
+            error = "EAN is required."
+
+        if error is not None:
+            flash(error)
+        else:
+            with pool.connection() as conn:
+                with conn.cursor(row_factory=namedtuple_row) as cur:
+                    cur.execute(
+                        """
+                       INSERT INTO product (sku, name, description, price, ean)
+                       VALUES (%(sku)s, %(name)s, %(description)s, %(price)s, %(ean)s);
+                       """,
+                        {"sku": sku, "name": name, "description": description, "price": price, "ean": ean},
+                    )
+                conn.commit()
+            return redirect(url_for("product_index"))
+
+    return render_template("product/add.html")
+
+
+@app.route("/product/<sku>/update", methods=("GET", "POST"))
+def product_update(sku):
+    """Update the product."""
+
+    with pool.connection() as conn:
+        with conn.cursor(row_factory=namedtuple_row) as cur:
+            product = cur.execute(
+                """
+               SELECT sku, name, description, price, ean
+               FROM product
+               WHERE sku = %(sku)s;
+               """,
+                {"sku": sku},
+            ).fetchone()
+            log.debug(f"SKU")
+
+    if request.method == "POST":
+
+        try:
+            price = float(request.form["price"])
+        except ValueError as e:
+            error = "Price must be numeric."
+
+        description = request.form["description"]
+
+        error = None
+
+        if not price:
+            error = "Price is required."
+
+        if not description:
+            error = "Description is required."
+
+        if error is not None:
+            log.debug(error)
+            flash(error)
+        else:
+            with pool.connection() as conn:
+                with conn.cursor(row_factory=namedtuple_row) as cur:
+                    cur.execute(
+                        """
+                       UPDATE product
+                       SET price = %(price)s,
+                       description = %(description)s
+                       WHERE sku = %(sku)s;
+                       """,
+                        {"sku": sku, "price": price, "description": description},
+                    )
+                conn.commit()
+            return redirect(url_for("product_index"))
+
+    
+    log.debug(product)
+
+    return render_template("product/update.html", product=product)
+
+@app.route("/product/delete/<sku>", methods=("GET",))
+def product_delete(sku):
+    """Delete the product."""
+
+    with pool.connection() as conn:
+        with conn.cursor(row_factory=namedtuple_row) as cur:
+            output = cur.execute(
+                """
+                DELETE FROM product
+                WHERE sku = %(sku)s;
+                """,
+                {"sku": sku},
+            )
+            log.debug(output)
+        conn.commit()
+    return redirect(url_for("product_index"))
+
+
+#---------------------------------------
+#               Supplier
+#----------------------------------------
+
+
+@app.route("/supplier", methods=("GET", "POST"))
+def supplier_index():
     """Supplier management page."""
-    return render_template("supplier/index.html")
+    with pool.connection() as conn:
+        with conn.cursor(row_factory=namedtuple_row) as cur:
+            cur.execute(
+                """
+               SELECT tin, name, address, sku, date
+               FROM supplier
+               ORDER BY name ASC;
+               """,
+                {},
+                # prepare=True,
+            )
+            suppliers = cur.fetchmany(10)
+            log.debug(len(suppliers))
+    return render_template("supplier/index.html", suppliers=suppliers)
 
+
+@app.route("/supplier/add", methods=("GET", "POST"))
+def supplier_add():
+    if request.method == "POST":
+        tin = request.form["tin"]
+        name = request.form["name"]
+        address = request.form["address"]
+        sku = request.form["sku"]
+        date = request.form["date"]
+
+        error = None
+
+        if not tin:
+            error = "TIN is required."
+
+        if not name:
+            error = "Name is required."
+
+        if not address:
+            error = "Address is required."
+
+        if not sku:
+            error = "SKU is required."
+
+        if not date:
+            error = "date is required."
+
+        if error is not None:
+            flash(error)
+        else:
+            with pool.connection() as conn:
+                with conn.cursor(row_factory=namedtuple_row) as cur:
+                    cur.execute(
+                        """
+                       INSERT INTO supplier (tin, name, address, sku, date)
+                       VALUES (%(tin)s, %(name)s, %(address)s, %(sku)s, %(date)s);
+                       """,
+                        {"tin": tin, "name": name, "address": address, "sku": sku, "date": date},
+                    )
+                conn.commit()
+            return redirect(url_for("supplier_index"))
+
+    return render_template("supplier/add.html")
+
+@app.route("/supplier/delete/<tin>", methods=("GET",))
+def supplier_delete(tin):
+    """Delete the supplier."""
+
+    with pool.connection() as conn:
+        with conn.cursor(row_factory=namedtuple_row) as cur:
+            cur.execute(
+                """
+                    DELETE FROM delivery
+                    WHERE tin = %(tin)s;
+                """,
+                {"tin": tin},  
+            )
+            
+            cur.execute(
+                """
+                    DELETE FROM supplier
+                    WHERE tin = %(tin)s;
+                """,
+                {"tin": tin},
+            )
+            
+        conn.commit()
+    return redirect(url_for("supplier_index"))
 
 # TODO. maybe merge with order.
 @app.route("/payment", methods=("GET",))
@@ -97,8 +302,8 @@ def payment():
 
 
 # @app.route("/aaaa", methods=("GET",))
-##@app.route("/accounts", methods=("GET",))
-# def indexaaa():
+# @app.route("/accounts", methods=("GET",))
+# def index():
 #    """Show all the accounts, most recent first."""
 
 #    with pool.connection() as conn:
